5133da1185ce362719e769a63571c14019623007
schiem <schiem01@gmail.com>
2013-11-14 18:08:55 -0600

    Added file transfer

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import sys
+import struct
 
 '''
 Command line arguments.
@@ -24,23 +25,53 @@ if '-a' in sys.argv:
 else:
     HOST = 'localhost'
 
+def put(file_name):
+    file_string = open(file_name, "rb").read()
+    file_size = len(file_string)
+    return struct.pack('>I', file_size) +  file_string
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-while 1:
-    command = raw_input('>')
-    s.sendall(command)
-    data = s.recv(1024)
-    if data == "exit":
-        break
-    if command.split()[0] == "grab":
-        try:
+def get_file(sock):
+    full_message = recvall(sock, 4)
+    if not full_message:
+        return None
+    mess_length = struct.unpack('>I', full_message)[0]
+    return recvall(sock, mess_length)
+
+def recvall(sock, n):
+    data = ''
+    while len(data) < n:
+        print "Receiving data of size " + str(n-len(data))
+        piece = sock.recv(n - len(data))
+        print "Data received."
+        if not piece:
+            print "No data received."
+            return None
+        data += piece
+    return data
+
+def open_connection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    return s
+
+if __name__ == "__main__":
+    s = open_connection()
+    while 1:
+        command = raw_input('>')
+        s.sendall(command)
+        if command.split()[0] == "grab":
+            data = get_file(s)
             open(command.split()[1], "wb").write(data)
-        except:
-            print "Could not add file."
-    else:
-        print data
-    if data == "exit":
-        break
+            data = s.recv(1024)
+            
+        elif command.split()[0] == "put":
+            s.sendall(put(command.split()[1]))
+            data = "Send successfully"
+        else:    
+            data = s.recv(1024)
+        if data == "exit":
+            break
+        else:
+            print data
 
-s.close()
+    s.close()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import os
 import sys
 import subprocess
+import struct
 
 HOST = ''
 
@@ -20,36 +21,60 @@ else:
 
 
 def grab(file_name):
-    try:
-        return open(file_name, "rb").read()
-    except:
-        return ''
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
+    file_string = open(file_name, "rb").read()
+    file_size = len(file_string)
+    return struct.pack('>I', file_size) +  file_string
 
-while 1:
-    data = conn.recv(1024)
-    if not data:
-        break
-    if data == "exit":
-        conn.sendall(data)
-        break
+def get_file(sock):
+    full_message = recvall(sock, 4)
+    if not full_message:
+        return None
+    mess_length = struct.unpack('>I', full_message)[0]
+    return recvall(sock, mess_length)
 
-    try:
-        if data.split()[0] == "grab":
-            out = grab(data.split()[1])
-        else:
-            out = subprocess.check_output(data, stderr=subprocess.STDOUT, shell=True)
-            if not out:
-                out = "Done."
-            if data.split()[0] == "cd":
-                os.chdir(data.split()[1])
-    except Exception, e:
-        out = str(e.output)
-    conn.sendall(out)
-
-conn.close()
+def recvall(sock, n):
+    data = ''
+    while len(data) < n:
+        piece = sock.recv(n - len(data))
+        if not piece:
+            return None
+        data += piece
+    return data
 
 
+def open_connection(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(1)
+    conn, addr = s.accept()
+    return s, conn, addr
+
+
+if __name__ == "__main__":
+    s, conn, addr = open_connection(HOST, PORT)
+    while 1:
+        data = conn.recv(1024)
+        if not data:
+            break
+        if data == "exit":
+            conn.sendall(data)
+            break
+
+        try:
+            if data.split()[0] == "grab":
+                out = grab(data.split()[1])
+            elif data.split()[0] == "put":
+                data = get_file(conn)
+                open(command.split()[1], "wb").write(data)
+                out = 'Success'
+            else:
+                out = subprocess.check_output(data, stderr=subprocess.STDOUT, shell=True)
+                if not out:
+                    out = "Done."
+                if data.split()[0] == "cd":
+                    os.chdir(data.split()[1])
+        except Exception, e:
+            out = str(e)
+        conn.sendall(out)
+
+    conn.close()
